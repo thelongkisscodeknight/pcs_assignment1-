e86e2a0d453b490de1b150cb9fa250a0b45e8f79
Captain Gabriel Angelos <captain@captain-Aspire-5535.(none)>
2013-10-08 00:00:11 -0700

    final commit

---
+++ variables.js
@@ -0,0 +1,12 @@
+var firstName = 'Fernando'
+var lastName = 'Horner"
+var fullName = firstName + ' ' + lastName
+ 
+var aNumber = 22
+var bNumber = 23
+var cNumber = aNumber + bNumber
+console.log (cNumber)
+
+var dNumber = (aNumber*bNumber) / cNumber
+console.log (dNumber)
+
